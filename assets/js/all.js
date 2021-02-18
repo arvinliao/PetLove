afd0d67891421516313b1bc0736ecc676e7a6452
arvinliao <LKS800507@gmail.com>
2021-02-18 16:03:14 +0800

    first commit

---
+++ assets/js/all.js
@@ -0,0 +1,41 @@
+"use strict";
+
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  }
+});
+"use strict";
+
+AOS.init({
+  offset: 200,
+  // offset (in px) from the original trigger point
+  delay: 0,
+  // values from 0 to 3000, with step 50ms
+  duration: 800 // values from 0 to 3000, with step 50ms
+
+});
+"use strict";
+
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  }
+});
+//# sourceMappingURL=all.js.map
